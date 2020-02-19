3e87b7f724d6bfebb1c3d5d6732a85e1ed684893
Oscar Van Slijpe <30955488+boscar98@users.noreply.github.com>
2020-02-19 17:37:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "xc.h"
 #include "init.h"
 #include "LIBPIC30.h"
+#include "math.h"
 
 void init_led(void){
     TRISBbits.TRISB10 = 0;
@@ -29,10 +30,29 @@ void init_adc(void){
     AD1CON1bits.SSRC = 2;   //Tringer sur la clock T3
 }
 
-void init_clock(void){
+void init_clock_smart(long freq){
+    long freq_base = 40000000;
+    long scale = 0;
+    while (freq_base/((pow(8,scale))*freq) > pow(2,16)){
+        scale ++;
+    }
+    T3CONbits.TCKPS = scale;
+    PR3 = freq_base/(pow(8,scale)*freq);
     T3CONbits.TON = 1;
+}
+
+void init_clock(void){
     PR3 = 2500; 
     T3CONbits.TCKPS = 2;
+}
+
+void init_uart(){
+    RPINR18bits.U1RXR = 7; //pour l'entrée, le multiplexeur est dans du périphérique uart, et on y met le numéro du pin
+    RPOR3bits.RP6R = 3; //pour la sortie, le multiplexeur est dans le pin, et on y met le numéro du périphérique uart
+    U1MODEbits.BRGH=0;
+    U1BRG = (40000000)/(16*115200)-1;
+    U1MODEbits.UARTEN = 1; //reception par défault
+    U1STAbits.UTXEN = 1; //emission aussi (dans cet ordre))
 }
 
 void config_led(int ADC1BUF0){
@@ -57,20 +77,33 @@ int main(void) {
     oscillatorInit();
     init_led();
     init_adc();
-    init_clock();
-    
-    while (1) {
+    init_uart();
+    init_clock_smart(5000);
+    while (1){
+    int i = 0;
+    while (U1STAbits.URXDA == 0){
+                
+    }
+    U1RXREG;
+    T3CONbits.TON = 1;
+    while (i<20000) {
         if (IFS0bits.T3IF == 1){
             IFS0bits.T3IF = 0;
             while (!AD1CON1bits.DONE){
-                
+            
             }
             if (AD1CON1bits.DONE){
-                clear_led();
-                config_led(ADC1BUF0);
+                while (U1STAbits.UTXBF==1){
+                    
+                }
+                ADC1BUF0>>4;
+                U1TXREG = ADC1BUF0;
             }
-        //reste du code à exécuter
-        }  
+        i++;
+        }    
+        
+    }
+    T3CONbits.TON = 0;
     }
     return 0;
 }
